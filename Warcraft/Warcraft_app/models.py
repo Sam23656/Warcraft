ad14b31d78d674052716cb87c7d49ff7c9d1b5ea
Sam23656 <sem26m06@gmail.com>
2023-09-13 19:36:49 +0500

    Update

--- Warcraft/Warcraft_app/models.py
+++ Warcraft/Warcraft_app/models.py
@@ -9,9 +9,15 @@ class Video(models.Model):
     category = models.CharField(max_length=20, choices=categorys)
     video = models.FileField(upload_to='videos/')
 
+    def __str__(self):
+        return self.name
+
 
 class Audio(models.Model):
     name = models.CharField(max_length=100)
     categorys = (("Music", 'Музыка'), ("Replicas", 'Реплики'))
     category = models.CharField(max_length=20, choices=categorys)
     audio = models.FileField(upload_to='audios/')
+
+    def __str__(self):
+        return self.name
